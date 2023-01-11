4b948965b8941b0542b9f62398dc1d692f1faaff
KASHI TAMANG <tamang.kashi@gmail.com>
2023-01-11 11:11:58 -0800

    comlpetes 3 functions

--- functions.js
+++ functions.js
@@ -1,7 +1,9 @@
 export function myFunction() {
     return true;
 }
-// This function should take a string and add return an new string that adds three exclamation points to the argument.
+
+// This function should take a string and add return an new string
+// that adds three exclamation points to the argument.
 /* 
 
 Input:
@@ -14,8 +16,8 @@ Output:
 
 */
 
-export function addExclamationPoints() {
-    return true;
+export function addExclamationPoints(string) {
+    return `${string}!!!`;
 }
 
 // This function should take in a number and multiply it by seven
@@ -31,7 +33,7 @@ Output:
 */
 
 export function multiplyBySeven(num) {
-    return true;
+    return num * 7;
 }
 
 // This function should take in a number and multiply it by twelve then cut the result in half
@@ -48,7 +50,7 @@ Output:
 */
 
 export function multiplyBy12ThenHalve(num) {
-    return true;
+    return (num * 12) / 2;
 }
 
 // This function should take in three numbers, divide the first by the second, then multiply the result by the third
@@ -162,7 +164,9 @@ Output:
 */
 
 export function renderDogLI(dog) {
-    return true;
+    const li = document.createElement('li');
+    li.textContent = dog.name;
+    return li;
 }
 
 // This function should take a dog object and return a div with the dog's information
--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,11 @@
 // IMPORT MODULES under test here:
-import { myFunction } from '../functions.js';
+import {
+    myFunction,
+    addExclamationPoints,
+    multiplyBySeven,
+    multiplyBy12ThenHalve,
+    renderDogLI,
+} from '../functions.js';
 
 const { test, skip } = QUnit;
 
@@ -11,10 +17,42 @@ test('this test should pass', (expect) => {
     expect.equal(actual, expected, 'true = true');
 });
 
-skip('this test should be skipped', (expect) => {
-    const expected = true;
+test('should return the with exclamation points', (expect) => {
+    const string = 'bunny';
 
-    const actual = true;
+    const expected = 'bunny!!!';
+
+    const actual = addExclamationPoints(string);
+
+    expect.equal(actual, expected);
+});
+
+test('should multiple by seven', (expect) => {
+    const num = 5;
+
+    const expected = 35;
+
+    const actual = multiplyBySeven(num);
+
+    expect.equal(actual, expected);
+});
+
+skip('should return dog name in an li', (expect) => {
+    const dog = { name: 'Benny', age: 6 };
+
+    const expected = '<li>Benny</li>';
+
+    const actual = renderDogLI(dog);
+
+    expect.equal(actual, expected);
+});
+
+test('should multiple by 12 then /2', (expect) => {
+    const num = 4;
+
+    const expected = 24;
+
+    const actual = multiplyBy12ThenHalve(num);
 
     expect.equal(actual, expected);
 });
